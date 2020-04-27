e63cfc05318c0a25c3f144331ebfacf8cbcba8a0
Paul Rinaldi <paulrinaldi10@yahoo.com>
2020-04-26 22:10:03 -0500

    implements api call in javascript, uses java to execute javascript on browser object

--- src/maps/client.js
+++ src/maps/client.js
@@ -1,18 +1,23 @@
-class Client {
-    static start() {
-        new Client();
-    }
+// document on ready -- this can probably be all updated to JS6...
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.querySelectorAll('#map').length > 0)
+  {
+    // appends the google maps api to html header
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD3Jb1jp2-D5Xi0kfEL5jEATK4c34nUDCY&callback=initMap';
+    document.getElementsByTagName('head')[0].appendChild(js_file);
+  }
+});
 
-    constructor() {
-        $(document).ready(function() {
-            onLoad();
-        });
+// global on the dom so that we can access it later through java easily
+var map; // holds out google map (on the div with id #map)
 
-        // https://www.google.com/maps/embed/v1/MODE?key=YOUR_API_KEY&parameters
-
-        let onLoad = function() {
-            alert("TEST ALERT");
-
-        };
-    }
+// global so that the callback in the google maps api async call can access this function
+function initMap() {
+    // constructs a GoogleMap map on the map object at the div with id=map at MSOE's (lat,lng)
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43.0469577, lng: -87.90786},
+        zoom: 8
+    });
 }
